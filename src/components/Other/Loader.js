868c5cfd15d562d03c99c8ac213102b57278b4b3
ilKhr <khorishko.ilja@yandex.ru>
2020-05-18 01:40:08 +0300

    bg preloader

--- src/components/Other/Loader.js
+++ src/components/Other/Loader.js
@@ -4,11 +4,13 @@ function Loader(){
 
 
     return(
-        <div className="container">
-            <div className="item item-1"></div>
-            <div className="item item-2"></div>
-            <div className="item item-3"></div>
-            <div className="item item-4"></div>
+        <div className="background">
+            <div className="container">
+                <div className="item item-1"></div>
+                <div className="item item-2"></div>
+                <div className="item item-3"></div>
+                <div className="item item-4"></div>
+            </div>
         </div>
     )
 }
