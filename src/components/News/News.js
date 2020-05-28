9597968bbc2b1dd1fe9df30fd665396271b24353
Alexandr-Darvin <aleksandr-darvin@mail.ru>
2020-05-29 01:00:53 +0300

    adaptiv news

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,6 +1,5 @@
 import './News.css'
 import ph3 from './photo3.png';
-import Carousel from './Carousel.js';
 import Text from './Text1.js';
 import React from 'react';
 import Header from '../Header/Header'
@@ -14,15 +13,12 @@ function News() {
             <div className='News'>
                 <div className="content">
 
-                    <div className="carousel">
-                        <Carousel/>
-                    </div>
+
 
                     <div className="card-news-body">
                         <img src={ph3} className="news-img" alt="иконка новости"></img>
                         <h5 className="news-text">НОВОСТИ</h5>
                     </div>
-                    <hr></hr>
 
                     <div className="text">
                         <Text/>
