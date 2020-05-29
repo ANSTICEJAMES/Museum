b4488de5f90ea8c64e52b5462d1c798e6e73aa86
ANSTICEJAMES <34317399+ANSTICEJAMES@users.noreply.github.com>
2020-05-29 22:03:25 +0300

    adaptive caregory V2, right title, background-color with name exhibit

--- src/components/Catalog/Element.js
+++ src/components/Catalog/Element.js
@@ -5,19 +5,21 @@ import './Element.css'
 
 function Element(props) {
 
-    const {name,image} = props.exhibit;
+    const { name, image } = props.exhibit;
 
-      
-        return (
-            <li>
-                <div className="card">
-                    <img className="card-img-top" alt="img" src={`${process.env.REACT_APP_API_URL}${image[0]}`}/>
-                    <div className="card-body">
-                        <h5 className="card-title"> {name}</h5>
-                    </div>
-                </div>
-            </li>
-        )
-    }
+
+    return (
+        <li>
+            <figure>
+          
+                <img alt="img" src={`${process.env.REACT_APP_API_URL}${image[0]}`} />
+                <figcaption><h5> {name}</h5></figcaption>
+               
+            </figure>
+           
+            
+        </li>
+    )
+}
 
 export default Element;
